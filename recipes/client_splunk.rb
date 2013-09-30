1519ff4a502832105031a1e3f844ff0506c89be4
Robert <taeil.choi@kt.com>
2013-09-30 14:26:06 +0900

    rename attr namespace

--- recipes/client_splunk.rb
+++ recipes/client_splunk.rb
@@ -35,10 +35,10 @@ cookbook_file 'splunk_rb' do
   mode   0755
 end
 
-if node.has_key?('monitoring') and node['monitoring'].has_key?('splunk')
+if node.has_key?('monitor') and node['monitor'].has_key?('splunk')
   cron 'splunk_cron' do
     minute "*/2"
-    command "#{ node['collectd']['plugin_dir'] }/splunk.rb #{ node['monitoring']['splunk']['ip'] } #{ node['monitoring']['splunk']['port'] } >/dev/null 2>&1"
+    command "#{ node['collectd']['plugin_dir'] }/splunk.rb #{ node['monitor']['splunk']['ip'] } #{ node['monitor']['splunk']['port'] } >/dev/null 2>&1"
     action :create
   end
 end
