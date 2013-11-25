281ecfb451977812b2a5d46caef3b5d5b52e7d9c
Robert <taeil.choi@kt.com>
2013-11-25 17:41:28 +0900

    change read_threads to 10

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,3 +22,4 @@ include_attribute "collectd"
 default['collectd']['version'] = "4.10.1"
 default['collectd']['server_recipe'] = "ktc-monitor\\:\\:server_collectd"
 default['collectd']['interval'] = 60
+default['collectd']['read_threads'] = 10
