29e403e94b2e5801662ef802a94d16049512f5f6
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2014-01-11 07:24:57 +0900

    auto_version to 0.9.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "taeilchoi1@gmail.com"
 license           "Apache 2.0"
 description       "KTC wrapper for collectd cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.6'
+version '0.9.7'
 
 recipe            "default", ""
 recipe            "client_collectd", "Install and configure collectd-client"
